67648a66f1e1fcf2618c9a1076790239f4d27c1b
Parth978 <parthasarathi978@gmail.com>
2024-03-07 00:53:17 +0530

    Day 6 Completed

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/';
+
+export const LOGO_URL =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTl6o4KSdaf0oI2JHs6jkWiH8UqT9RMOfOSw&usqp=CAU';
